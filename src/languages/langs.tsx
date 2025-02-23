f2d8adab610aa56d57a875208fc6b3d68c955cf6
franciscoblancojn <blancofrancisco34@gmail.com>
2025-02-23 17:16:51 -0500

    flb

--- src/languages/langs.tsx
+++ src/languages/langs.tsx
@@ -4,55 +4,133 @@ export type LIST_LANGS_TYPE = ['es', 'en'];
 export const LIST_LANGS: LIST_LANGS_TYPE = ['es', 'en'];
 
 export const LANGUAGE_LIST: LanguageListProps<LIST_LANGS_TYPE> = {
-    'Gracias por usar Fenextjs. Esta plantilla tiene una estructura que te ayudará a usar fenextjs.':
-        {
-            es: 'Gracias por usar Fenextjs. Esta plantilla tiene una estructura que te ayudará a usar fenextjs.',
-            en: 'Thank you for using Fenextjs. This template has a structure that will help you use Fenextjs.',
-        },
-    'Recuerda que puedes agregar todos los componentes de fenextjs, que necesites, a la carpeta ui-fenextjs, para personalizarlo en estilos o agregarle mas funcionalidades':
-        {
-            es: 'Recuerda que puedes agregar todos los componentes de fenextjs, que necesites, a la carpeta ui-fenextjs, para personalizarlo en estilos o agregarle mas funcionalidades',
-            en: 'Remember that you can add all the Fenextjs components you need to the ui-fenextjs folder to customize it with styles or add more functionalities.',
-        },
-    'Esta plantilla tiene un ejemplo de cómo crear un componente de login y un sistema simple de traducciones.':
-        {
-            es: 'Esta plantilla tiene un ejemplo de cómo crear un componente de login y un sistema simple de traducciones.',
-            en: 'This template has an example of how to create a login component and a simple translation system.',
-        },
-    'Fenextjs Documentación': {
-        es: 'Fenextjs Documentación',
-        en: 'Fenextjs Documentation',
+    Deinsa: {
+        es: 'Deinsa',
+        en: 'Deinsa',
     },
-    Correo: {
-        es: 'Correo',
-        en: 'Email',
+    'Este es el Deinsa.': {
+        es: 'Este es el Deinsa.',
+        en: 'This is Deinsa.',
+    },
+    'Espero les guste la integracion que hice con Nextjs 14, typescript, tailwindcss y fenextjs':
+        {
+            es: 'Espero les guste la integracion que hice con Nextjs 14, typescript, tailwindcss y fenextjs',
+            en: 'I hope you like the integration I made with Next.js 14, TypeScript, TailwindCSS, and Fenext.js.',
+        },
+    'Pueden usar este usuario para acceder:': {
+        es: 'Pueden usar este usuario para acceder:',
+        en: 'You can use this user to access:',
+    },
+    Usuario: {
+        es: 'Usuario',
+        en: 'User',
     },
     Contraseña: {
         es: 'Contraseña',
         en: 'Password',
     },
-    Enviar: {
-        es: 'Enviar',
-        en: 'Submit',
-    },
-    'Correo requerido': {
-        es: 'Correo requerido',
-        en: 'Email required',
-    },
-    'Correo invalido': {
-        es: 'Correo invalido',
-        en: 'Email invalid',
-    },
-    'Contraseña requerida': {
-        es: 'Contraseña requerida',
-        en: 'Password required',
+    'Fenextjs Documentación': {
+        es: 'Fenextjs Documentación',
+        en: 'Fenext.js Documentation',
     },
     Entrar: {
-        en: 'Login',
         es: 'Entrar',
+        en: 'Login',
+    },
+    Correo: {
+        es: 'Correo',
+        en: 'Email',
+    },
+    Enviar: {
+        es: 'Enviar',
+        en: 'Send',
+    },
+    Registrate: {
+        es: 'Registrate',
+        en: 'Sign up',
+    },
+    'Olvidates la Contraseña': {
+        es: 'Olvidates la Contraseña',
+        en: 'Forgot your password?',
+    },
+    Nombre: {
+        es: 'Nombre',
+        en: 'Name',
+    },
+    'Repite la Contraseña': {
+        es: 'Repite la Contraseña',
+        en: 'Repeat Password',
+    },
+    'Recuperar Contraseña': {
+        es: 'Recuperar Contraseña',
+        en: 'Recover Password',
+    },
+    Salir: {
+        es: 'Salir',
+        en: 'Logout',
     },
     Idiomas: {
         es: 'Idiomas',
         en: 'Languages',
+    },
+    Responsables: {
+        es: 'Responsables',
+        en: 'Managers',
+    },
+    'Aqui puedes mirar tus Responsables.': {
+        es: 'Aqui puedes mirar tus Responsables.',
+        en: 'Here you can see your managers.',
+    },
+    Clear: {
+        es: 'Limpiar',
+        en: 'Clear',
+    },
+    Estatus: {
+        es: 'Estatus',
+        en: 'Status',
+    },
+    ID: {
+        es: 'ID',
+        en: 'ID',
+    },
+    Rol: {
+        es: 'Rol',
+        en: 'Role',
+    },
+    'Fecha de Creacion': {
+        es: 'Fecha de Creacion',
+        en: 'Creation Date',
+    },
+    'Fecha de Actualizacion': {
+        es: 'Fecha de Actualizacion',
+        en: 'Update Date',
+    },
+    Activo: {
+        es: 'Activo',
+        en: 'Active',
+    },
+    Inactivo: {
+        es: 'Inactivo',
+        en: 'Inactive',
+    },
+    Eliminado: {
+        es: 'Eliminado',
+        en: 'Deleted',
+    },
+    Responsable: {
+        es: 'Responsable',
+        en: 'Manager',
+    },
+    Email: {
+        es: 'Email',
+        en: 'Email',
+    },
+    'Fecha de creacion': {
+        es: 'Fecha de creacion',
+        en: 'Creation Date',
+    },
+    'Fecha de actualizacion': {
+        es: 'Fecha de actualizacion',
+        en: 'Update Date',
     },
 };
